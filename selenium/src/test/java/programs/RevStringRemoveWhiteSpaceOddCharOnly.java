59fa030dfcabd32c333f0552fa42e49a9db8c96e
sauravnayak999@gmail.com <sauravnayak999@gmail.com>
2022-08-12 01:24:04 +0530

    A String is given remove the white spaces, reverse it and print the only
     odd position characters.

---
+++ selenium/src/test/java/programs/RevStringRemoveWhiteSpaceOddCharOnly.java
@@ -0,0 +1,35 @@
+package programs;
+
+import java.util.Collections;
+import java.util.Scanner;
+
+public class RevStringRemoveWhiteSpaceOddCharOnly {
+		
+	
+	
+	// . A String is given remove the white spaces, 
+	// reverse it and print the only odd position characters.
+	
+	public static void main(String[] args) {
+		
+		String str= "This is a test Automation Practice Question";
+		//Automation Practice Question
+		String[] s= str.split(" ");
+		String temp="";
+		for(String i:s) {
+			temp=i+temp;
+		}
+		
+		String res="";
+		
+		System.out.println("After Removing Whitespace and reversing String: "+str+":  is:  "+temp);
+		for(int j=0;j<temp.length();j=j+2) {
+			res=res+temp.charAt(j);
+		}
+		System.out.println("The only odd character String of: "+temp+" : is:  "+res);
+		
+		
+
+	}
+
+}
